39b229600b29a0f8b48524b19183248773a2e277
Emil Ivanov <emoemo2535@gmail.com>
2024-10-01 12:35:19 +0300

    variables, constants and data types

--- Section2/src/VariableFun.java
+++ Section2/src/VariableFun.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class VariableFun {
+    public static void main(String[] args) {
+        int age; //declaration
+        age = 19; //initialization
+
+        String town = "Sofia";
+
+        final int SOME_NUM = 150;
+
+        String name = "Billy";
+
+//        System.out.println(name + " is " + age + "!");
+        System.out.println(name + " is " + age + "!");
+        System.out.print("Town: " + town);
+    }
 }
